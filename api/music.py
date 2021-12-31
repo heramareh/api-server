e9f41552763b70f76ed185be421acea814ff36ed
heramareh <8926924@qq.com>
2021-12-31 21:27:00 +0800

    1

--- api/music.py
+++ api/music.py
@@ -3,6 +3,10 @@
 # @File   : music.py
 # @Author : liwang
 # @Time   : 2021-12-26 19:07
+import json
+import re
+import time
+import requests
 from flask_restful import Resource, request
 from flask import jsonify
 import os
@@ -14,10 +18,47 @@ class MusicList(Resource):
         datas = []
         # print(request.args.get('path'))
         # path = request.args.get('path')
-        path = "/home/liwang/FrontendProject/vuetest/static"
+        path = "E:\\FrontendProject\\vuetest\\static"
+        # path = "/home/liwang/FrontendProject/vuetest/static"
         for root, dir, files in os.walk(path):
             for id, file in enumerate(files, 1):
                 datas.append({"id": id, "name": file, "src": './static/'+file})
+        return jsonify({"code": 200, "data": datas})
+
+class SearchMusic(Resource):
+
+    def __search(self, song_name):
+        """搜索歌曲"""
+        search_url = "https://songsearch.kugou.com/song_search_v2?callback=jQuery112405132987859127838_{}&page" \
+                     "=1&pagesize=30&userid=-1&clientver=&platform=WebFilter&tag=em&filter=2&iscorrection=1&privilege_fil" \
+                     "ter=0&_={}&keyword={}".format(str(int(time.time() * 1000)), str(int(time.time() * 1000)),
+                                                    song_name)
+        obj = requests.get(search_url)
+        start = re.search("jQuery\d+_\d+\(?", obj.text)
+        data = json.loads(obj.text.strip().lstrip(start.group()).rstrip(")"))
+        return data['data']['lists']
+
+    def __get_src(self, song_info):
+        """下载歌曲"""
+        # 展示前十个搜索结果
+        print(str(song_info['FileName']).replace('<em>', '').replace('</em>', ''))
+        file_hash = song_info['FileHash']
+        url = "http://m.kugou.com/app/i/getSongInfo.php?cmd=playInfo&hash={}".format(file_hash)
+        obj = requests.get(url)
+        data = obj.json()  # json格式
+        return data['url']
+
+    def get(self):
+        datas = []
+        id = 1
+        name = request.args.get('name').strip()
+        song_list = self.__search(name)
+        if song_list:
+            for song_info in self.__search(name)[:10]:
+                src = self.__get_src(song_info)
+                if src:
+                    datas.append({"id": id, "name": str(song_info['FileName']).replace('<em>', '').replace('</em>', ''), "src": src})
+                    id += 1
         return jsonify({"code": 200, "data": datas})
 
 if __name__ == '__main__':
--- app.py
+++ app.py
@@ -7,12 +7,13 @@ from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
 from api.hello_world import HelloWorld
-from api.music import MusicList
+from api.music import MusicList, SearchMusic
 
 if __name__ == '__main__':
     app = Flask(__name__)
     CORS(app, supports_credentials=True)
     api = Api(app)
     api.add_resource(HelloWorld, '/api/helloworld')
-    api.add_resource(MusicList, '/api/musiclist')
+    api.add_resource(MusicList, '/api/music/list')
+    api.add_resource(SearchMusic, '/api/music/search')
     app.run(debug=True, host='0.0.0.0', port=8888)
